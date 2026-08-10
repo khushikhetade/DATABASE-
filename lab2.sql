03ebff00a33f6da71a7b959df66878f3a057bfd3
Khushi Khetade <Khushikhetade76@gmail.com>
2026-08-10 10:47:56 +0530

    Secure Banking Data Model Implementation & Banking Business Rules Enforcement

---
+++ lab2.sql
@@ -0,0 +1,103 @@
+create database BankingDB;
+use BankingDB;
+
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Phone VARCHAR(15),
+    AccountCreationDate DATE
+);
+
+CREATE TABLE Accounts (
+    AccountID INT,
+    AccountType VARCHAR(20),
+    Balance DECIMAL(10,2)
+);
+
+CREATE TABLE Transactions (
+    TransactionID INT,
+    TransactionDate DATE,
+    Amount DECIMAL(10,2),
+    TransactionType VARCHAR(20)
+);
+
+CREATE TABLE Branches (
+    BranchID INT,
+    BranchName VARCHAR(100),
+    BranchAddress VARCHAR(200),
+    BranchPhone VARCHAR(15)
+);
+
+CREATE TABLE AccountBranches ( 
+		AssignmentDate DATE
+);
+
+CREATE TABLE Loans (
+    LoanID INT,
+    LoanAmount DECIMAL(10,2),
+    InterestRate DECIMAL(5,2),
+    StartDate DATE,
+    EndDate DATE
+);
+
+ALTER TABLE Customers
+ADD DateOfBirth DATE;
+
+ALTER TABLE Customers
+MODIFY Phone VARCHAR(20);
+
+ALTER TABLE Accounts
+ADD CONSTRAINT chk_MinBalance
+CHECK (Balance >= 1000);
+
+DROP TABLE AccountBranches;
+
+ALTER TABLE Accounts
+ADD CustomerID INT;
+
+ALTER TABLE Accounts
+ADD CONSTRAINT FK_Accounts_Customers
+FOREIGN KEY (CustomerID)
+REFERENCES Customers(CustomerID);
+
+ALTER TABLE Customers
+MODIFY FirstName VARCHAR(50) NOT NULL;
+
+ALTER TABLE Customers
+ADD CONSTRAINT uq_Email UNIQUE (Email);
+
+ALTER TABLE Loans 
+ADD CustomerID INT ;
+
+ALTER TABLE Loans 
+ADD CONSTRAINT fk_Loans_Customers
+FOREIGN KEY (CustomerID)
+REFERENCES Customers(CustomerID);
+
+ALTER TABLE Accounts 
+ADD BranchID int ;
+
+ALTER TABLE Branches 
+ADD PRIMARY KEY (BranchID);
+
+ALTER TABLE Accounts 
+ADD CONSTRAINT fk_Accounts_Branches
+FOREIGN KEY (BranchID)
+REFERENCES Branches(BranchID);
+
+ALTER TABLE Transactions 
+ADD AccountID INT ;
+
+ALTER TABLE Accounts
+ADD PRIMARY KEY (AccountID);
+
+ALTER TABLE Transactions
+ADD CONSTRAINT fk_Transaction_Accounts
+FOREIGN KEY (AccountID)
+REFERENCES Accounts(AccountID);
+
+
+
+
