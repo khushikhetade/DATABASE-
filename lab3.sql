ca0ae0c0e27510d23ddd63e02f4d1a0351f931d7
Khushi Khetade <Khushikhetade76@gmail.com>
2026-08-10 10:54:34 +0530

    Initialize Banking Data with Insert, Update & Delete Operations

---
+++ lab3.sql
@@ -0,0 +1,137 @@
+create database BankingDB;
+use BankingDB;
+
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Phone VARCHAR(15),
+    AccountCreationDate DATE
+);
+
+CREATE TABLE Accounts (
+    AccountID INT,
+    AccountType VARCHAR(20),
+    Balance DECIMAL(10,2)
+);
+
+CREATE TABLE Transactions (
+    TransactionID INT,
+    TransactionDate DATE,
+    Amount DECIMAL(10,2),
+    TransactionType VARCHAR(20)
+);
+
+CREATE TABLE Branches (
+    BranchID INT,
+    BranchName VARCHAR(100),
+    BranchAddress VARCHAR(200),
+    BranchPhone VARCHAR(15)
+);
+
+CREATE TABLE AccountBranches ( 
+		AssignmentDate DATE
+);
+
+CREATE TABLE Loans (
+    LoanID INT,
+    LoanAmount DECIMAL(10,2),
+    InterestRate DECIMAL(5,2),
+    StartDate DATE,
+    EndDate DATE
+);
+
+ALTER TABLE Customers
+ADD DateOfBirth DATE;
+
+ALTER TABLE Customers
+MODIFY Phone VARCHAR(20);
+
+ALTER TABLE Accounts
+ADD CONSTRAINT chk_MinBalance
+CHECK (Balance >= 1000);
+
+DROP TABLE AccountBranches;
+
+ALTER TABLE Accounts
+ADD CustomerID INT;
+
+ALTER TABLE Accounts
+ADD CONSTRAINT FK_Accounts_Customers
+FOREIGN KEY (CustomerID)
+REFERENCES Customers(CustomerID);
+
+ALTER TABLE Customers
+MODIFY FirstName VARCHAR(50) NOT NULL;
+
+ALTER TABLE Customers
+ADD CONSTRAINT uq_Email UNIQUE (Email);
+
+ALTER TABLE Loans 
+ADD CustomerID INT ;
+
+ALTER TABLE Loans 
+ADD CONSTRAINT fk_Loans_Customers
+FOREIGN KEY (CustomerID)
+REFERENCES Customers(CustomerID);
+
+ALTER TABLE Accounts 
+ADD BranchID int ;
+
+ALTER TABLE Branches 
+ADD PRIMARY KEY (BranchID);
+
+ALTER TABLE Accounts 
+ADD CONSTRAINT fk_Accounts_Branches
+FOREIGN KEY (BranchID)
+REFERENCES Branches(BranchID);
+
+ALTER TABLE Transactions 
+ADD AccountID INT ;
+
+ALTER TABLE Accounts
+ADD PRIMARY KEY (AccountID);
+
+ALTER TABLE Transactions
+ADD CONSTRAINT fk_Transaction_Accounts
+FOREIGN KEY (AccountID)
+REFERENCES Accounts(AccountID);
+
+INSERT INTO Customers
+(CustomerID, FirstName, LastName, Email, Phone, DateOfBirth)
+VALUES
+(101,'Rahul','Sharma','rahul@gmail.com','9876543210','1998-04-15');
+
+INSERT INTO Accounts
+(AccountID, CustomerID, AccountType, Balance)
+VALUES
+(201,101,'Savings',25000);
+
+SET SQL_SAFE_UPDATES=0;
+
+UPDATE Customers
+SET Phone='9999999999'
+WHERE CustomerID=101;
+
+SELECT * FROM Customers
+WHERE CustomerID = 101;
+
+UPDATE Customers
+SET Email='rahul.sharma@gmail.com'
+WHERE CustomerID=101;
+
+SELECT * FROM Customers
+WHERE CustomerID = 101;
+
+DELETE FROM Transactions
+WHERE TransactionID = 302;
+
+SELECT * FROM Transactions;
+
+DELETE FROM Accounts
+WHERE AccountID = 202;
+
+SELECT * FROM Accounts;
+
+
