a9644822c70f632512f1ec799d161fcff5fc6485
Khushi Khetade <Khushikhetade76@gmail.com>
2026-08-24 12:56:34 +0530

    Generate Banking Summary Report using Aggregate

---
+++ lab6.sql
@@ -0,0 +1,420 @@
+create database BankingDB;
+use BankingDB;
+
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Phone VARCHAR(15),
+    AccountCreationDate DATE
+);
+
+CREATE TABLE Accounts (
+    AccountID INT,
+    AccountType VARCHAR(20),
+    Balance DECIMAL(10,2)
+);
+
+CREATE TABLE Transactions (
+    TransactionID INT,
+    TransactionDate DATE,
+    Amount DECIMAL(10,2),
+    TransactionType VARCHAR(20)
+);
+
+CREATE TABLE Branches (
+    BranchID INT,
+    BranchName VARCHAR(100),
+    BranchAddress VARCHAR(200),
+    BranchPhone VARCHAR(15)
+);
+
+CREATE TABLE AccountBranches ( 
+		AssignmentDate DATE
+);
+
+CREATE TABLE Loans (
+    LoanID INT,
+    LoanAmount DECIMAL(10,2),
+    InterestRate DECIMAL(5,2),
+    StartDate DATE,
+    EndDate DATE
+);
+
+ALTER TABLE Customers
+ADD DateOfBirth DATE;
+
+ALTER TABLE Customers
+MODIFY Phone VARCHAR(20);
+
+ALTER TABLE Accounts
+ADD CONSTRAINT chk_MinBalance
+CHECK (Balance >= 1000);
+
+DROP TABLE AccountBranches;
+
+ALTER TABLE Accounts
+ADD CustomerID INT;
+
+ALTER TABLE Accounts
+ADD CONSTRAINT FK_Accounts_Customers
+FOREIGN KEY (CustomerID)
+REFERENCES Customers(CustomerID);
+
+ALTER TABLE Customers
+MODIFY FirstName VARCHAR(50) NOT NULL;
+
+ALTER TABLE Customers
+ADD CONSTRAINT uq_Email UNIQUE (Email);
+
+ALTER TABLE Loans 
+ADD CustomerID INT ;
+
+ALTER TABLE Loans 
+ADD CONSTRAINT fk_Loans_Customers
+FOREIGN KEY (CustomerID)
+REFERENCES Customers(CustomerID);
+
+ALTER TABLE Accounts 
+ADD BranchID int ;
+
+ALTER TABLE Branches 
+ADD PRIMARY KEY (BranchID);
+
+ALTER TABLE Accounts 
+ADD CONSTRAINT fk_Accounts_Branches
+FOREIGN KEY (BranchID)
+REFERENCES Branches(BranchID);
+
+ALTER TABLE Transactions 
+ADD AccountID INT ;
+
+ALTER TABLE Accounts
+ADD PRIMARY KEY (AccountID);
+
+ALTER TABLE Transactions
+ADD CONSTRAINT fk_Transaction_Accounts
+FOREIGN KEY (AccountID)
+REFERENCES Accounts(AccountID);
+
+INSERT INTO Customers
+(CustomerID, FirstName, LastName, Email, Phone, DateOfBirth)
+VALUES
+(101,'Rahul','Sharma','rahul@gmail.com','9876543210','1998-04-15');
+
+INSERT INTO Accounts
+(AccountID, CustomerID, AccountType, Balance)
+VALUES
+(201,101,'Savings',25000);
+
+SET SQL_SAFE_UPDATES=0;
+
+UPDATE Customers
+SET Phone='9999999999'
+WHERE CustomerID=101;
+
+SELECT * FROM Customers
+WHERE CustomerID = 101;
+
+UPDATE Customers
+SET Email='rahul.sharma@gmail.com'
+WHERE CustomerID=101;
+
+SELECT * FROM Customers
+WHERE CustomerID = 101;
+
+DELETE FROM Transactions
+WHERE TransactionID = 302;
+
+SELECT * FROM Transactions;
+
+DELETE FROM Accounts
+WHERE AccountID = 202;
+
+SELECT * FROM Accounts;
+
+SELECT * FROM Customers;
+
+SELECT FirstName, LastName, Email, Phone
+FROM Customers;
+
+SELECT *
+FROM Accounts
+WHERE AccountType = 'Savings';
+
+SELECT *
+FROM Accounts
+WHERE Balance > 25000;
+
+SELECT *
+FROM Transactions
+WHERE Amount BETWEEN 5000 AND 20000;
+
+SELECT *
+FROM Customers
+WHERE CustomerID IN (101,102,103);
+
+SELECT *
+FROM Customers
+WHERE FirstName LIKE 'R%';
+
+SELECT *
+FROM Customers
+ORDER BY FirstName ASC;
+
+SELECT *
+FROM Accounts
+ORDER BY Balance DESC;
+
+SELECT DISTINCT AccountType
+FROM Accounts;
+
+SELECT *
+FROM Accounts
+ORDER BY Balance DESC
+LIMIT 3;
+
+SELECT *
+FROM Transactions
+LIMIT 5 OFFSET 2;
+
+SELECT *
+FROM Customers
+WHERE Phone IS NULL;
+
+SELECT *
+FROM Customers
+WHERE Email IS NOT NULL;
+
+SELECT AccountID,
+       Balance,
+       CASE
+           WHEN Balance >= 50000 THEN 'Premium Account'
+           WHEN Balance >= 25000 THEN 'Standard Account'
+           ELSE 'Basic Account'
+       END AS AccountCategory
+FROM Accounts;
+
+SELECT AccountID,
+       Balance,
+       RANK() OVER (ORDER BY Balance DESC) AS BalanceRank
+FROM Accounts;
+
+SELECT TransactionID,
+       Amount,
+       SUM(Amount) OVER (ORDER BY TransactionDate) AS RunningTotal
+FROM Transactions;
+
+SELECT TransactionID,
+       Amount,
+       AVG(Amount) OVER () AS AverageTransaction
+FROM Transactions;
+
+SELECT *
+FROM Customers
+WHERE FirstName LIKE 'A%';
+
+SELECT *
+FROM Customers
+WHERE Email LIKE '%gmail%';
+
+SELECT *
+FROM Customers
+WHERE LastName LIKE '%kar';
+
+SELECT *
+FROM Accounts
+WHERE AccountType IN ('Savings', 'Current');
+
+SELECT *
+FROM Transactions
+WHERE TransactionType IN ('Deposit', 'Withdrawal');
+
+SELECT *
+FROM Customers
+WHERE CustomerID IN (101,102,105);
+
+SELECT *
+FROM Customers
+ORDER BY LastName ASC;
+
+
+SELECT *
+FROM Accounts
+ORDER BY Balance DESC;
+
+
+SELECT *
+FROM Transactions
+ORDER BY TransactionDate DESC;
+
+SELECT *
+FROM Accounts
+ORDER BY Balance DESC
+LIMIT 5;
+
+SELECT *
+FROM Customers
+LIMIT 3;
+
+SELECT *
+FROM Transactions
+LIMIT 5 OFFSET 3;
+
+SELECT *
+FROM Accounts
+WHERE AccountType = 'Savings'
+ORDER BY Balance DESC;
+
+SELECT *
+FROM Customers
+WHERE FirstName LIKE 'S%'
+LIMIT 5;
+
+SELECT *
+FROM Transactions
+WHERE TransactionType IN ('Deposit','Withdrawal')
+ORDER BY TransactionDate DESC;
+
+
+select * from customers;
+
+
+SELECT
+FirstName,
+UPPER(FirstName) AS UpperCaseName
+FROM customers;
+
+
+SELECT
+FirstName,
+LOWER(FirstName) AS LowerCaseName
+FROM customers;
+
+
+SELECT
+FirstName,
+LENGTH(FirstName) AS NameLength
+FROM customers;
+
+
+SELECT
+FirstName,
+LEFT(FirstName,3) AS Initials
+FROM customers;
+
+SELECT
+CONCAT(FirstName,' - ',LastName) AS FullName
+FROM customers;
+
+
+
+SELECT ROUND(1256.75) AS Rounded_Value;
+
+
+SELECT CEIL(1256.25) AS Ceiling_Value;
+
+SELECT FLOOR(1256.75) AS Floor_Value;
+
+
+SELECT ABS(-2500) AS Absolute_Value;
+
+
+SELECT MOD(25,4) AS Remainder;
+
+
+select * from customers;
+
+SELECT CURDATE(); 
+
+SELECT NOW();
+
+SELECT
+CustomerID,
+YEAR(DateOfBirth) AS BirthYear
+FROM customers;
+
+SELECT
+CustomerID,
+MONTH(DateOfBirth) AS BirthMonth
+FROM customers;
+
+
+SELECT
+CustomerID,
+DATEDIFF(CURDATE(),DateOfBirth) AS Days
+FROM customers;
+
+
+SELECT
+    FirstName,
+    DateOfBirth,
+    IF(YEAR(DateOfBirth) <= 1995,
+       'Adult',
+       'Young') AS Category
+FROM Customers;
+
+
+SELECT
+    FirstName,
+    IFNULL(Phone, 'Not Available') AS PhoneNumber
+FROM Customers;
+
+SELECT GREATEST(
+'2000-09-20',
+'1995-06-18',
+'1997-09-12',
+'1993-11-25'
+) AS LatestBirthDate;
+
+
+SELECT LEAST(
+'2000-09-20',
+'1995-06-18',
+'1997-09-12',
+'1993-11-25'
+) AS EarliestBirthDate;
+
+
+SELECT
+    FirstName,
+    NULLIF(FirstName,'Priya') AS Result
+FROM Customers;
+
+
+SELECT SUM(Balance) as total_balance
+FROM Accounts;
+
+
+SELECT AVG(Balance) AS average_balance
+FROM Accounts;
+
+
+SELECT MAX(Balance) AS highest_balance
+FROM Accounts;
+
+
+SELECT MIN(Balance) AS lowest_balance
+FROM Accounts;
+
+
+SELECT COUNT(*) AS total_accounts
+FROM Accounts;
+
+
+SELECT 
+    AccountType,
+    SUM(Balance) AS TotalBalance
+FROM Accounts
+GROUP BY AccountType ;
+
+
+SELECT 
+    AccountType,
+    SUM(Balance) AS TotalBalance
+FROM Accounts
+GROUP BY AccountType
+HAVING SUM(Balance) > 25000;
+
+
